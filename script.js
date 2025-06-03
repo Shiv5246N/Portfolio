138d08cc719f2509cc541a56046b2d60bfb72410
Shivang Raj Saxena <shivangrajsaxena6@gmail.com>
2025-06-03 16:14:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function f1(){
+var A = document.getElementById("A").value;
+console.log(A);
+window.open("https://github.com/Shiv5246N","_blank");
+}
+function f2(){
+var B = document.getElementById("B").value;
+console.log(B);
+window.open("https://cloud.google.com/discover/what-is-cloud-analytics","_blank");
+}
+function f3(){
+var C = document.getElementById("C").value;
+console.log(C);
+window.open("https://www.linkedin.com/in/shivang-raj-saxena-571a9a24b","_blank");
+}
+function f4(){
+var D = document.getElementById("D").value;
+console.log(D);
+window.open("https://x.com/saxena_shi38052","_blank");
+}
+function f5(){
+var E = document.getElementById("E").value;
+console.log(E);
+window.open("https://www.instagram.com/shivangrajsaxena_14/","_blank");
+}
